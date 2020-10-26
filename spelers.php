3e552b4036b335f7d0151251d49c606c468b87f8
CrispijnO <crispijnw@gmail.com>
2020-10-26 16:08:24 +0100

    eind van de dag commit. speler pagina tooltip on hover moet op andere plaats komen

---
+++ spelers.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="spelers.css">
+    <link rel="stylesheet" href="footer.css" />
+    <link rel="stylesheet" href="header.css">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+      rel="stylesheet"
+    />
+    <meta name="robots" content="all">
+    <meta name="language" content="Dutch">
+    <meta name="author" content="inowhare">
+    <meta name="description" content="sc hereveen">
+    <meta name="keywords" content="voetbal, hereveen, abe lenstra stadion">
+    <title>Spelers</title>
+</head>
+<body>
+    <header>
+      <div class="header">
+        <div class="heerenveen">
+          <img src="images/heerenveen-logo.png" alt="logo" class="foto">
+          <div class="logo-text">
+            SC Heerenveen
+          </div>
+          <a href="#">
+            <div class="sponsoren">
+              <img src="images/Ausnutria-logo.png" alt="hoofdsponsor" class="foto-hoofdsponsor">
+            </div>
+          </a>
+        </div>
+        <div class="background">
+          <div class="nav">
+            <div class="index">
+              NAVIGATIE
+            </div>
+            <a href="#">HOME</a>
+            <a href="#">WEDSTRIJDEN</a>
+            <a href="#">SPELERS</a>
+            <a href="#">BIER</a>
+            <a href="#">TICKETS</a>
+            <a href="#">SPONSOREN</a>
+            <a href="#">BESTUUR</a>
+            <a href="#">OVER ONS</a>
+            <a href="#">CONTACT</a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <nav>
+
+    </nav>
+    <main>
+        <div class="aanval">
+          <h2>Aanvallers</h2>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6513704bd1e5e.png" alt="Ulysses Llanez">
+              <figcaption>Ulysses Llanez</figcaption>
+              <span class="tooltipText">Naam: Ulysses Llanez<br>Geboortedatum: 02 april 2001<br>Rugnummer: 7<br>Gespeelde wedstrijden: 2</span>
+            </figure>
+          </div>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=65243897311a9.jpg" alt="Benjamin Nygren">
+              <figcaption>Benjamin Nygren</figcaption>
+            </figure>
+          </div>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6497986f94c8b.jpg" alt="Henk Veerman">
+              <figcaption>Henk Veerman</figcaption>
+            </figure>
+          </div>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64922e0ee29e8.png" alt="Mitchell van Bergen">
+              <figcaption>Mitchell van Bergen</figcaption>
+            </figure>
+          </div>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6492083eaa611.png" alt="Arjen van der Heide">
+              <figcaption>Arjen van der Heide</figcaption>
+            </figure>
+          </div>
+          <div class="aanvalSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=649298cf7e23d.png" alt="Rein Smit">
+              <figcaption>Rein Smit</figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="midden">
+          <h2>Middenvelders</h2>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6508701f0a256.png" alt="Oliver Batista Meier">
+              <figcaption>Oliver Batista Meier</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=652446b91a387.jpg" alt="Stanislav Shopov">
+              <figcaption>Stanislav Shopov</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491972de583a.png" alt="Sieben Dewaele">
+              <figcaption>Sieben Dewaele</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64925759f03c7.png" alt="Rami Hajal">
+              <figcaption>Rami Hajal</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64927deb97f87.png" alt="Hamdi Akujobi">
+              <figcaption>Hamdi Akujobi</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64928aef76b01.png" alt="Jan Ras">
+              <figcaption>Jan Ras</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64924bd4932f6.png" alt="Joey Veerman">
+              <figcaption>Joey Veerman</figcaption>
+            </figure>
+          </div>
+          <div class="middenSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6492614d26fa6.png" alt="Rodney Kongolo">
+              <figcaption>Rodney Kongolo</figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="verdedigen">
+          <h2>Verdedigers</h2>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64914320dba6f.png" alt="Sherel Floranus">
+              <figcaption>Sherel Floranus</figcaption>
+            </figure>
+          </div>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6515488aa9d00.png" alt="Jan Paul van Hecke">
+              <figcaption>Jan Paul van Hecke</figcaption>
+            </figure>
+          </div>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491736ddcae3.png" alt="Joaquin Fernandez">
+              <figcaption>Joaquin Fernandez</figcaption>
+            </figure>
+          </div>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64918d0b457a8.png" alt="Lucas Woudenberg">
+              <figcaption>Lucas Woudenberg</figcaption>
+            </figure>
+          </div>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64915bf11f4d0.png" alt="Ibrahim Dresevic">
+              <figcaption>Ibrahim Dresevic</figcaption>
+            </figure>
+          </div>
+          <div class="verdedigerSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=650885d6744fe.jpg" alt="Pawel Bochniewicz">
+              <figcaption>Pawel Bochniewicz</figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="keeper">
+          <h2>Keeper</h2>
+          <div class="keeperSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491386cc2cd1.png" alt="Erwin Mulder">
+              <figcaption>Erwin Mulder</figcaption>
+            </figure>
+          </div>
+          <div class="keeperSpeler">
+            <figure class="tooltip">
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64916da6d0537.png" alt="Jaimy Kroesen">
+              <figcaption>Jaimy Kroesen</figcaption>
+            </figure>
+          </div>
+        </div>
+    </main>
+    <footer>
+        <div class="mapouter">
+          <div class="gmap_canvas">
+            <iframe
+              width="100%"
+              height="300"
+              id="gmap_canvas"
+              src="https://maps.google.com/maps?q=abe%20lenstra%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
+          </div>
+        </div>
+        <div class="contactGegevens">
+          <h2>Contact</h2>
+          <p>
+            Email: 
+            <br>
+            placeholder@email.com
+            <br>
+            <br />
+            Telefoon: 
+            <br>
+            0515-415131 
+            <br>
+            <br />
+            Plaats:
+            <br>
+             Heerenveen
+          </p>
+        </div>
+        <div class="volgendeWedstrijd">
+          <h2>Volgende Wedstrijd</h2>
+          <p>
+            <?php
+              echo "23 December 2020
+              <br />
+              14:00
+              <br />
+              Sc Heerenveen - Feyenoord
+              <br>
+              <img class='firstLogo' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/SC_Heerenveen_logo.svg/1200px-SC_Heerenveen_logo.svg.png' alt='SC Heerenveen logo'><img class='secondLogo' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/Feyenoord_logo.svg/1200px-Feyenoord_logo.svg.png' alt='Feyenoord logo'>"
+            ?>
+          </p>
+        </div>
+      </footer>
+</body>
+</html>
