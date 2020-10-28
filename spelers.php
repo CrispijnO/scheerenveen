3a6f445ba6856d90b5497593e1456d3ec9919567
CrispijnO <crispijnw@gmail.com>
2020-10-28 13:13:55 +0100

    header aanpassingen, kleuren en sponsor logo. speler pagina is af. footer nu bij 740 en lager ipv 750 en lager

--- spelers.php
+++ spelers.php
@@ -27,7 +27,7 @@
           </div>
           <a href="#">
             <div class="sponsoren">
-              <img src="images/Ausnutria-logo.png" alt="hoofdsponsor" class="foto-hoofdsponsor">
+              <img src="images/Ausnutria-logo2.png" alt="hoofdsponsor" class="foto-hoofdsponsor">
             </div>
           </a>
         </div>
@@ -60,32 +60,37 @@
             </figure>
           </div>
           <div class="aanvalSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=65243897311a9.jpg" alt="Benjamin Nygren">
+              <span class="overlay">Naam:	Benjamin Nygren<br>Geboortedatum: 8 juli 2001<br>Rugnummer: 8<br>Gespeelde wedstrijden: 3</span>
               <figcaption>Benjamin Nygren</figcaption>
             </figure>
           </div>
           <div class="aanvalSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6497986f94c8b.jpg" alt="Henk Veerman">
+              <span class="overlay">Naam: Henk Veerman<br>Geboortedatum: 26 februari 1991<br>Rugnummer: 9<br>Gespeelde wedstrijden: 73</span>
               <figcaption>Henk Veerman</figcaption>
             </figure>
           </div>
           <div class="aanvalSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64922e0ee29e8.png" alt="Mitchell van Bergen">
+              <span class="overlay">Naam: Mitchell van bergen<br>Geboortedatum: 27 augustus 1999<br>Rugnummer: 11<br>Gespeelde wedstrijden: 68</span>
               <figcaption>Mitchell van Bergen</figcaption>
             </figure>
           </div>
           <div class="aanvalSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6492083eaa611.png" alt="Arjen van der Heide">
+              <span class="overlay">Naam: Arjen van der Heide<br>Geboortedatum: 19 november 2001<br>Rugnummer: 16<br>Gespeelde wedstrijden: 9</span>
               <figcaption>Arjen van der Heide</figcaption>
             </figure>
           </div>
           <div class="aanvalSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=649298cf7e23d.png" alt="Rein Smit">
+              <span class="overlay">Naam: Rein Smit<br>Geboortedatum: 5 januari 2001<br>Rugnummer: 26<br>Gespeelde wedstrijden: 2</span>
               <figcaption>Rein Smit</figcaption>
             </figure>
           </div>
@@ -93,50 +98,58 @@
         <div class="midden">
           <h2>Middenvelders</h2>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6508701f0a256.png" alt="Oliver Batista Meier">
+              <span class="overlay">Naam: Oliver Batista Meier<br>Geboortedatum: 16 februari 2001<br>Rugnummer: 10<br>Gespeelde wedstrijden: 6</span>
               <figcaption>Oliver Batista Meier</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=652446b91a387.jpg" alt="Stanislav Shopov">
+              <span class="overlay">Naam: Stanislav Shopov<br>Geboortedatum: 	23 februari 2002<br>Rugnummer: 13<br>Gespeelde wedstrijden: 0</span>
               <figcaption>Stanislav Shopov</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491972de583a.png" alt="Sieben Dewaele">
+              <span class="overlay">Naam: Sieben Dewaele<br>Geboortedatum: 2 februari 1999<br>Rugnummer: 15<br>Gespeelde wedstrijden: 4</span>
               <figcaption>Sieben Dewaele</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64925759f03c7.png" alt="Rami Hajal">
+              <span class="overlay">Naam: Rami Hajal<br>Geboortedatum: 17 september 2001<br>Rugnummer: 17<br>Gespeelde wedstrijden: 12</span>
               <figcaption>Rami Hajal</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64927deb97f87.png" alt="Hamdi Akujobi">
+              <span class="overlay">Naam: Hamdi Akujobi<br>Geboortedatum: 20 januari 2000<br>Rugnummer: 18<br>Gespeelde wedstrijden: 5</span>
               <figcaption>Hamdi Akujobi</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64928aef76b01.png" alt="Jan Ras">
+              <span class="overlay">Naam: Jan Ras<br>Geboortedatum: 28 januari 1999<br>Rugnummer: 19<br>Gespeelde wedstrijden: 3</span>
               <figcaption>Jan Ras</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64924bd4932f6.png" alt="Joey Veerman">
+              <span class="overlay">Naam: Joey Veerman<br>Geboortedatum: 19 november 1998<br>Rugnummer: 20<br>Gespeelde wedstrijden: 33</span>
               <figcaption>Joey Veerman</figcaption>
             </figure>
           </div>
           <div class="middenSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6492614d26fa6.png" alt="Rodney Kongolo">
+              <span class="overlay">Naam: Rodney Kongolo<br>Geboortedatum: 9 januari 1998<br>Rugnummer: 21<br>Gespeelde wedstrijden: 46</span>
               <figcaption>Rodney Kongolo</figcaption>
             </figure>
           </div>
@@ -144,38 +157,44 @@
         <div class="verdedigen">
           <h2>Verdedigers</h2>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64914320dba6f.png" alt="Sherel Floranus">
+              <span class="overlay">Naam: Sherel Floranus<br>Geboortedatum: 23 augustus 1998<br>Rugnummer: 2<br>Gespeelde wedstrijden: 57</span>
               <figcaption>Sherel Floranus</figcaption>
             </figure>
           </div>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6515488aa9d00.png" alt="Jan Paul van Hecke">
+              <span class="overlay">Naam: Jan Paul van Hecke<br>Geboortedatum: 8 juni 2000<br>Rugnummer: 3<br>Gespeelde wedstrijden: 5</span>
               <figcaption>Jan Paul van Hecke</figcaption>
             </figure>
           </div>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
-              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491736ddcae3.png" alt="Joaquin Fernandez">
-              <figcaption>Joaquin Fernandez</figcaption>
+            <figure>
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491736ddcae3.png" alt="Joaquín Fernández">
+              <span class="overlay">Naam: Joaquín Fernández<br>Geboortedatum: 22 januari 1999<br>Rugnummer: 4<br>Gespeelde wedstrijden: 0</span>
+              <figcaption>Joaquín Fernández</figcaption>
             </figure>
           </div>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64918d0b457a8.png" alt="Lucas Woudenberg">
+              <span class="overlay">Naam: Lucas Woudenberg<br>Geboortedatum: 25 april 1994<br>Rugnummer: 5<br>Gespeelde wedstrijden: 75</span>
               <figcaption>Lucas Woudenberg</figcaption>
             </figure>
           </div>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
-              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64915bf11f4d0.png" alt="Ibrahim Dresevic">
-              <figcaption>Ibrahim Dresevic</figcaption>
+            <figure>
+              <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64915bf11f4d0.png" alt="Ibrahim Drešević">
+              <span class="overlay">Naam: Ibrahim Drešević<br>Geboortedatum: 24 januari 1997<br>Rugnummer: 14<br>Gespeelde wedstrijden: 42</span>
+              <figcaption>Ibrahim Drešević</figcaption>
             </figure>
           </div>
           <div class="verdedigerSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=650885d6744fe.jpg" alt="Pawel Bochniewicz">
+              <span class="overlay">Naam: Pawel Bochniewicz<br>Geboortedatum: 30 januari 1996<br>Rugnummer: 25<br>Gespeelde wedstrijden: 7</span>
               <figcaption>Pawel Bochniewicz</figcaption>
             </figure>
           </div>
@@ -183,14 +202,16 @@
         <div class="keeper">
           <h2>Keeper</h2>
           <div class="keeperSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=6491386cc2cd1.png" alt="Erwin Mulder">
+              <span class="overlay">Naam: Erwin Mulder<br>Geboortedatum: 3 maart 1989<br>Rugnummer: 1<br>Gespeelde wedstrijden: 7</span>
               <figcaption>Erwin Mulder</figcaption>
             </figure>
           </div>
           <div class="keeperSpeler">
-            <figure class="tooltip">
+            <figure>
               <img src="https://www.sc-heerenveen.nl/includes/resize.asp?website=implementatie&image=64916da6d0537.png" alt="Jaimy Kroesen">
+              <span class="overlay">Naam: Jaimy Kroesen<br>Geboortedatum: 27 maart 2003<br>Rugnummer: 23<br>Gespeelde wedstrijden: 0</span>
               <figcaption>Jaimy Kroesen</figcaption>
             </figure>
           </div>
