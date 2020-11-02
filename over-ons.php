d5061b043294593fa68ec5b3aeb0eb4112a18bba
supr3mion <230690@edu.rocfriesepoort.nl>
2020-11-02 12:41:05 +0100

    over ons pagina toegevoegd, meer foto's toegevoegd

---
+++ over-ons.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta http-equiv="Content-Type"
+        content="text/html";
+        charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="over-ons.css">
+        <link rel="stylesheet" type="text/css" href="footerheader.css">
+        <meta name="robots" content="all">
+        <meta name="language" content="Dutch">
+        <meta name="author" content="innoware">
+        <meta name="description" content="sc hereveen">
+        <meta name="keywords" content="voetbal, hereveen, abe lenstra stadion">
+        <title>sc hereveen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    </head>
+    <body>
+        <div class="header">
+            <div class="heerenveen">
+                <img src="images/heerenveen-logo.png" alt="logo" class="foto">
+                <div class="logo-text">
+                SC Heerenveen
+                </div>
+                <a href="#">
+                <div class="sponsoren">
+                    <img src="images/Ausnutria-logo.png" alt="hoofdsponsor" class="foto-hoofdsponsor">
+                </div>
+                </a>
+            </div>
+            <div class="background">
+            <div class="nav">
+                <div class="index">php</div>
+                <a href="#">HOME</a>
+                <a href="#">WEDSTRIJDEN</a>
+                <a href="#">SPELERS</a>
+                <a href="#">BIER</a>
+                <a href="#">TICKETS</a>
+                <a href="#">SPONSOREN</a>
+                <a href="#">BESTUUR</a>
+                <a href="#">OVER ONS</a>
+                <a href="#">CONTACT</a>
+            </div>
+            </div>
+        </div>
+        <div class="content-container">
+          <div class="info-container">
+          <p class="titel">algemeen</p><br>
+          <p class="info">
+            Sportclub Heerenveen is een Nederlandse profvoetbalclub uit Heerenveen, 
+            die uitkomt in de Eredivisie van zowel de heren- als vrouwencompetitie. 
+            De grootste successen die de club heeft behaald zijn het bereiken van 
+            de groepsfase van de UEFA Champions League in 2000-2001 en het winnen 
+            van de KNVB beker in 2009.<br><br>
+            De thuisbasis van sc Heerenveen is het Abe Lenstra Stadion, 
+            vernoemd naar de oud-voetballer Abe Lenstra die in 1985 overleed.
+            Er kunnen momenteel 27.224 toeschouwers plaatsnemen in het 
+            stadion. Dit betekent in theorie dat bijna alle inwoners van 
+            de plaats Heerenveen (32.200 inwoners) als toeschouwer in het 
+            stadion plaats kunnen nemen.
+          </p><br><br><br>
+          <p class="titel">Geschiedenis</p><br>
+          <p class="info">
+            In 1902 werd in Heerenveen gevoetbald door de club Victorie. 
+            Er ontstonden verschillende elftallen en verenigingen die 
+            tegen andere steden en dorpen uitkwamen. Door samengaan van 
+            Victorie en de Heerenveense vereniging Achilles ontstond in 
+            1908 de club V.A.C. Deze club ging in 1918 ter ziele wegens 
+            de mobilisatie, echter was het een kwestie van tijd dat de 
+            club opnieuw werd opgericht onder een andere naam.
+          </p><br><br><br>
+          <p class="titel">Oprichting</p><br>
+          <p class="info">
+            De club sc Heerenveen ontstond op 20 juli 1920 onder de 
+            naam Athleta; in het seizoen 1921/22 werd de naam gewijzigd 
+            in Spartaan. Toen de club circa een jaar later toetrad tot 
+            de competitie van de Nederlandse Voetbal Bond, kreeg ze de 
+            naam VV Heerenveen, omdat er al andere clubs waren, die 
+            onder de naam (De) Spartaan speelden. Na splitsing in 
+            afdelingen voor amateur- en betaald voetbal veranderde de 
+            naam van de club op 1 juni 1977 in sc Heerenveen.
+          </p><br><br><br>
+          <p class="titel">Het stadion</p><br>
+          <p class="info">
+            Sinds 1994 speelt sc Heerenveen haar thuiswedstrijden in 
+            het nieuwe Abe Lenstra stadion (huidige capaciteit ruim 
+            26.100 plaatsen). Er is sinds dat jaar veel gebouwd, 
+            verbouwd aan het stadion. Het bleek wegens het succes 
+            van de club al gauw te klein voor onder andere supporters, 
+            sponsors, personeel, kleedruimtes en technische staf.
+          </p><br><br><br>
+          <p class="titel">foto's</p><br>
+        </div>
+        <div class="foto-container">
+          <img src="images/img1.jpg">
+          <img src="images/img2.jpg">
+          <img src="images/img3.jpg">
+          <img src="images/img4.jpg">
+          <img src="images/img5.jpg">
+          <img src="images/img8.jpg">
+          <img src="images/img7.jpg">
+          <img src="images/img6.jpg">
+          <img src="images/img9.jpg">
+          <img src="images/img10.jpg">
+          <img src="images/img11.jpg">
+        </div>
+            </div>
+        <footer>
+            <div class="mapouter">
+              <div class="gmap_canvas">
+                <iframe
+                  width="100%"
+                  height="300"
+                  id="gmap_canvas"
+                  src="https://maps.google.com/maps?q=abe%20lenstra%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                ></iframe>
+              </div>
+            </div>
+            <div class="contactGegevens">
+              <h2>Contact</h2>
+              <p>
+                Email: 
+                <br>
+                placeholder@email.com
+                <br>
+                <br />
+                Telefoon: 
+                <br>
+                0515-415131 
+                <br>
+                <br />
+                Plaats:
+                <br>
+                 Heerenveen
+              </p>
+            </div>
+            <div class="volgendeWedstrijd">
+              <h2>Volgende Wedstrijd</h2>
+              <p>
+                <?php
+                  echo "23 December 2020
+                  <br />
+                  14:00
+                  <br />
+                  Sc Heerenveen - Feyenoord
+                  <br>
+                  <img class='firstLogo' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/SC_Heerenveen_logo.svg/1200px-SC_Heerenveen_logo.svg.png' alt='SC Heerenveen logo'><img class='secondLogo' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/Feyenoord_logo.svg/1200px-Feyenoord_logo.svg.png' alt='Feyenoord logo'>"
+                ?>
+              </p>
+            </div>
+          </footer>
+    </body>
+</html>
